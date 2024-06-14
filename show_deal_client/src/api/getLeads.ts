dc7bce0fd4d942d799044151f54939c574e6c725
AbibullaevAR <a.alim2019@mail.ru>
2024-06-14 12:02:03 +0300

    Set prod url in getLeads client

--- show_deal_client/src/api/getLeads.ts
+++ show_deal_client/src/api/getLeads.ts
@@ -2,7 +2,7 @@ import { Lead } from "@/types/lead";
 
 export const getLeads = async (query = ''): Promise<Lead[]> => {
   try {
-    const response = await fetch(`/api/leads?query=${query}`);
+    const response = await fetch(`https://showdeals-production.up.railway.app/api/leads?query=${query}`);
     
     return response.json();
   } catch (e) {
